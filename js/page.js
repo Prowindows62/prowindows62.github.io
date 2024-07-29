c20396fc2af7bfcc8fe5306e2221a755bf095de3
WindowsFan9600 <prowindows62@gmail.com>
2024-07-29 23:16:45 +0300

    Update data

---
+++ js/page.js
@@ -0,0 +1,32 @@
+function showContent(page) {
+    var contentDiv = document.getElementById('content');
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        contentDiv.innerHTML = xhr.responseText;
+      }
+    };
+    
+    // Load content based on page parameter
+    switch (page) {
+      case 'home':
+        xhr.open('GET', 'home.html', true);
+        break;
+      case 'about':
+        xhr.open('GET', 'about.html', true);
+        break;
+      case 'services':
+        xhr.open('GET', 'services.html', true);
+        break;
+      case 'servicestor':
+        xhr.open('GET', 'servicestor.html', true);
+        break;
+      default:
+        xhr.open('GET', 'home.html', true); // Default to home page
+    }
+    
+    xhr.send();
+  }
+  
+  // Initial page load
+  showContent('home');
